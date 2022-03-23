9e56c6d8c997bd12caca572f5dd006bb8e23ab9f
Tobi Okanlawon <tobiinlondon34@gmail.com>
2022-03-23 20:53:33 +0100

    Fix problem with statistics using stale data

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,17 +1,17 @@
 import { useState } from 'react';
 
-const Statistics = ({good, neutral, bad, all, average, positive, display}) => {
+const Statistics = ({feedback, statistics, display}) => {
     if(display){
         return(
             <>
               <h2>statistics</h2>
               <div>
-                <p>good { good }</p>
-                <p>neutral { neutral }</p>
-                <p>bad { bad }</p>
-                <p>all {all}</p>
-                <p>average {average}</p>
-                <p>positive {positive}%</p>
+                <StatisticsLine text={'good'} value={feedback.good} />
+                <StatisticsLine text={'neutral'} value={feedback.neutral} />
+                <StatisticsLine text={'bad'} value={feedback.bad} />
+                <StatisticsLine text={'all'} value={statistics.all} />
+                <StatisticsLine text={'average'} value={statistics.average}/>
+                <StatisticsLine text={'positive'} value={statistics.positive + '%'} />
               </div>
             </>
         );
@@ -25,62 +25,85 @@ const Statistics = ({good, neutral, bad, all, average, positive, display}) => {
     };
 };
 
+const Button = ({clickHandler, text}) => {
+    return (
+        <button onClick={clickHandler}>
+          {text}
+        </button>
+    );
+};
+
+    const StatisticsLine = ({text, value}) => {
+    return (
+        <>
+          <p>{text} {value}</p>
+        </>
+    );
+};
 
 const App = () => {
     // save clicks of each button to its own state
-    const [good, setGood] = useState(0);
-    const [neutral, setNeutral] = useState(0);
-    const [bad, setBad] = useState(0);
-    const [all, setAll] = useState(good+neutral+bad);
-    const [average, setAverage] = useState(0);
-    const [positive, setPositive] = useState(0);
+    const [feedback, setFeedback] = useState({
+        good: 0,
+        neutral: 0,
+        bad: 0,
+    });
+    
+    const [statistics, setStatistics] = useState({
+	average: 0,
+	positive: 0,
+	all: 0,
+    });
+    
     const [display, setDisplay] = useState(false);
 
-    const recomputeOtherValues = function(){
-        let numberOfAll = good + neutral + bad;
-        setAverage((good * 1) + (neutral * 0) + (bad * -1)/numberOfAll || 1);
-        setPositive((good/numberOfAll || 1)*100);
-        // divide by one to avoid division by zero and the NaN problem
-        setAll(numberOfAll);
+    const computeStastics = function(newFeedback){
+        const newStatistics = {};
+        // 'all' has to be computed first.
+        newStatistics.all = newFeedback.good + newFeedback.bad + newFeedback.neutral;
+        newStatistics.average = ((newFeedback.good * 1) + (newFeedback.bad * -1)) / newStatistics.all;
+        newStatistics.positive = newFeedback.good * 100 / newStatistics.all;
+
+        setStatistics(newStatistics);
     };
+    
     const giveGoodFeedback = function(){
-        setGood(good + 1);
-        recomputeOtherValues();
-        return;
+        const newFeedback = {...feedback, good: feedback.good + 1};
+        computeStastics(newFeedback);
+        setFeedback(newFeedback);
+        setDisplay(true);
     };
 
     const giveNeutralFeedback = function(){
-        setNeutral(neutral + 1);
-        recomputeOtherValues();
-        return;
+        const newFeedback = {...feedback, neutral: feedback.neutral + 1};
+        computeStastics(newFeedback);
+        setFeedback(newFeedback);
+        setDisplay(true);
     };
 
     const giveBadFeedback = function(){
-        setBad(bad + 1);
-        recomputeOtherValues();
-        return;
+        const newFeedback = {...feedback, bad: feedback.bad + 1};
+        computeStastics(newFeedback);
+        setFeedback(newFeedback);
+        setDisplay(true);
     };
 
     return (
-        <div>
+        <>
           <div>
             <h2>Give Feedback</h2>
             <div>
-              <button onClick={giveGoodFeedback}>good</button>
-              <button onClick={giveNeutralFeedback}>neutral</button>
-              <button onClick={giveBadFeedback}>bad</button>
+              <Button text={'good'} clickHandler={giveGoodFeedback} />
+              <Button text={'neutral'} clickHandler={giveNeutralFeedback} />
+              <Button text={'bad'} clickHandler={giveBadFeedback} />
             </div>
             <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              all={all}
-              average={average}
-              positive={positive}
+              feedback={feedback}
+	      statistics={statistics}
               display={display}
             />
           </div>
-        </div>
+        </>
     );
 };
 
