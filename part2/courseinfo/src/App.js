b3adbbfdee9e3927ca46ed9b0969882592bcb9fa
Tobi Okanlawon <tobiinlondon34@gmail.com>
2022-04-13 12:52:24 +0100

    Do step 9 of courseinfo

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,6 +1,6 @@
-const Header = ({ course }) => <h1>{course}</h1>;
+const Header = ({ course }) => <h2>{course}</h2>;
 
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+const Total = ({ sum }) => <strong>total of exercises {sum}</strong>;
 
 const Part = ({ part }) => 
       <p>
@@ -34,37 +34,68 @@ const Course = ({course, total})=> {
 };
 
 const App = () => {
-    const course = {
-        id: 1,
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10,
-                id: 1
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7,
-                id: 2
-            },
-            {
-                name: 'State of a component',
-                exercises: 14,
-                id: 3
-            }
-        ]
+    const courses = [
+        {
+            id: 1,
+            name: 'Half Stack application development',
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                }
+            ],
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ];
+
+    const total = function({parts}){
+        console.log(parts);
+        return parts.reduce(
+            function(previousValue, currentValue){
+                return previousValue + currentValue.exercises;
+            }, 0
+        );
     };
 
-    const total = course.parts.reduce(
-        function(previousValue, currentValue){
-            return previousValue + currentValue.exercises;
-        },
-        0);
-
-    return <Course
-             course={course}
-             total={total}
-           />;
+    let returnList = courses.map((entry, index) => {
+        return <Course
+                 course={entry}
+                 total={total(entry)}
+                 key={entry.id}
+               />;
+    });
+    
+    return (
+        <>
+          <h1>Web Development Curriculum</h1>
+          {returnList}
+        </>
+    );
 };
 export default App;
