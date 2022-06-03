2182d812e3a9332863006e2cca922aae7cad6b66
Tobi Okanlawon <tobiinlondon34@gmail.com>
2022-06-03 21:47:17 +0100

    Do exercise 2.12: Data for Countries, Step 1
    There is a bug, admittedly, in the code. Sometimes the warning to the user to pick another filter behaves erratically. I think I just need to keep working on other exercises, I'll find the bug later.

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -7,8 +7,7 @@ const Search = ({country, handleCountryInputChange, responseToInput}) => {
           <p>find countries: 
             <input
               value={country}
-              onChange={handleCountryInputChange} />
-            
+              onChange={handleCountryInputChange} />            
           </p>
           {responseToInput}
         </div>
@@ -30,13 +29,19 @@ const Country = ({country}) => {
         <div>
           {console.log(country)}
           <h2>{country.name.common}</h2>
-          <p>Capital: {country.capital}</p>
+          <p>Capital: </p>
+            <ul>
+              { country.capital.map((capital) => <li key={capital}>{capital}</li>)}
+            </ul>
           <p>Area: {country.area}</p>
 
           <h3>Langauges</h3>
           <ul>
-            {country.languages.map((language) => <li key={language.name}> {language.name} </li>)}
-              </ul>
+            {
+                Object.values(country.languages).map((language) => <li key={language}> {language} </li>)
+            }
+          </ul>
+          <img src={country.flags.png} alt={`flag of ${country.name.common}`}/>
         </div>
     );
 };
@@ -46,15 +51,17 @@ const App = () => {
     const [country, setCountry] = useState('');
     const [responseToInput, setResponseToInput] = useState('');
     const [searchResult, setSearchResult] = useState([]);
-    const [allCountries, setAllCountries] = useState([
-        {name: {common: 'Nigeria'}}, {name: {common: 'Niger'}}, {name: {common: 'Spain'}}
-    ]);
+    const [allCountries, setAllCountries] = useState([]);
 
     const handleCountryInputChange = (e) => {
         setCountry(e.target.value);
 
+        // if the country data isn't done loading yet
+        if(!allCountries.length) return;
+
         const searchResult = handleSearch(e.target.value);
-        if(searchResult.length > 10) setResponseToInput('Too many matches, specify another filter');
+        if(searchResult.length > 10 && !(country.length === 0) ) setResponseToInput('Too many matches, specify another filter');
+        if(searchResult.length < 10) setResponseToInput('');
         
         setSearchResult(searchResult);
     };
@@ -80,19 +87,20 @@ const App = () => {
         }
         else if(searchResult.length === 1) {
             return (
-                <Country country={searchResult}/>
+                <Country country={searchResult[0]}/>
             );
         };
         return (
             <p>No results</p>
         );
     };
-    // useEffect( () => {
-    //     axios.get('https://restcountries.com/v3.1/all')
-    //         .then((res)=>{
-    //             setAllCountries(res.data);
-    //         });
-    // }, []);
+    useEffect( () => {
+        axios.get('https:restcountries.com/v3.1/all')
+            .then((res)=>{
+                setAllCountries(res.data);
+                console.log('ready for use');
+            });
+    }, []);
 
     return (
         <div>
